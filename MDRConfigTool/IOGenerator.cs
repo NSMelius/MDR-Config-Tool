eb895f16be8ece4ea158cb04eeebbac21c92ec67
Charlie <charliezmuda@gmail.com>
2020-04-27 14:20:33 -0500

    Testing and Adding references

--- MDRConfigTool/IOGenerator.cs
+++ MDRConfigTool/IOGenerator.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCatSysManagerLib;
+using EnvDTE;
 
 namespace MDRConfigTool
 {
-    class IOGenerator
+    public class IOGenerator
     {
+        public static ITcSysManager11 sysMan;
+        public static EnvDTE.Project pro;
+        public static EnvDTE.Solution sol;
+        public static EnvDTE.DTE dte;
+
+
     }
 }
