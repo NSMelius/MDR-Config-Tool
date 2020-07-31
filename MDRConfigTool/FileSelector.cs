d0bf3d089764b1ef8d8c929c74dc7b86a40c1871
Charlie <charliezmuda@gmail.com>
2020-07-31 16:25:41 -0500

    link variables updated

--- MDRConfigTool/FileSelector.cs
+++ MDRConfigTool/FileSelector.cs
@@ -94,7 +94,15 @@ namespace MDRConfigTool
 
         private void btnLibNext_Click(object sender, EventArgs e)
         {
-         solHandler.
+            button1_Click(sender, e);
+            btnOPenFile_Click(sender, e);
+            solHandler = new SolutionHandler();
+            pnlWelcome.Visible = false;
+            pnlSolutionsettings.Visible = true;
+
+            solHandler.PLCdeclarations(DT);
+
+            solHandler.linkVariables(DT);
         }
     }
 }
